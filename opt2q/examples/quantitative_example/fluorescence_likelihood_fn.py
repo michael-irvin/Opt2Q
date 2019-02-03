aa95b925c56eb5c824b421b518567422dd73850c
michael-irvin <michael.w.irvin@vanderbilt.edu>
2019-02-02 19:54:06 -0600

    Updated fluorescence_likelihood_fn [skip ci]

--- opt2q/examples/quantitative_example/fluorescence_likelihood_fn.py
+++ opt2q/examples/quantitative_example/fluorescence_likelihood_fn.py
@@ -15,24 +15,22 @@ file_path = os.path.join(script_dir, 'fluorescence_data.csv')
 
 raw_fluorescence_data = pd.read_csv(file_path)
 fluorescence_data = raw_fluorescence_data[['# Time', 'norm_IC-RP', 'nrm_var_IC-RP', 'norm_EC-RP', 'nrm_var_EC-RP']]\
-    .rename(columns={'# Time': 'time'})  # Remove unnecessary whitespace
-
-# convert time scale to that of simulation
-fluorescence_data['time'] = fluorescence_data['time'].apply(lambda x: x*500.0/3600)
+    .rename(columns={'# Time': 'time'})  # Remove unnecessary whitespace in column name
 
 dataset = DataSet(fluorescence_data[['time', 'norm_IC-RP', 'norm_EC-RP']],
-                  measured_variables={'norm_IC-RP': 'semi-quantitative', 'norm_EC-RP': 'semi-quantitative'})
+                  measured_variables={'norm_IC-RP': 'semi-quantitative',
+                                      'norm_EC-RP': 'semi-quantitative'})
 dataset.measurement_error_df = fluorescence_data[['nrm_var_IC-RP', 'nrm_var_EC-RP']].\
-    rename(columns={'nrm_var_IC-RP': 'norm_IC-RP__error', 'nrm_var_EC-RP': 'norm_EC-RP__error'})
+    rename(columns={'nrm_var_IC-RP': 'norm_IC-RP__error',
+                    'nrm_var_EC-RP': 'norm_EC-RP__error'})  # DataSet expects error columns to have "__error" suffix
 
 # ------- Parameters --------
-parameters = pd.DataFrame([[1.0, 1.0]], columns=['kc3', 'kc4'])
+# The model is sensitive to these parameters
+parameters = pd.DataFrame([[1e4, 1e-2, 1e-2, 1e-6, 1e-6]], columns=['C_0', 'kc3', 'kc4', 'kf3', 'kf4'])
 
 # ------- Dynamics -------
 sim = Simulator(model=model, param_values=parameters, solver='scipyode')
-results = sim.run(np.linspace(0, 5000, 100))
-results_df = results.opt2q_dataframe
-results_df = results_df.reset_index()
+results = sim.run(np.linspace(0, 21600, 100))
 
 # ------- Measurement -------
 fl = Fluorescence(results,
@@ -45,15 +43,17 @@ measurement_results = fl.run()
 
 @objective_function(simulator=sim, measurement_model=fl, return_results=False, evals=0)
 def likelihood_fn(x):
-    kc3 = 10 ** x[0]                                                        # float [(-3, 3),
-    kc4 = 10 ** x[1]                                                        # float  (-3, 3),
-    l_0 = 10 ** x[2]  # value of corresponding to the 1 ng/ml TRAIL         # float  (1, 3),
+    C_0 = 10 ** x[0]        # float     [( 2, 6),
+    kc3 = 10 ** x[1]        # float      (-2, 4),
+    kc4 = 10 ** x[2]        # float      (-2, 4),
+    kf3 = 10 ** x[3]        # float      (-8,-4),
+    kf4 = 10 ** x[4]        # float      (-8,-4),
 
-    params = pd.DataFrame([[kc3, kc4, l_0*50]], columns=['kc3', 'kc4', 'L_0'])
+    params = pd.DataFrame([[C_0, kc3, kc4, kf3, kf4]], columns=['C_0', 'kc3', 'kc4', 'kf3', 'kf4'])
     likelihood_fn.simulator.param_values = params
 
     # dynamics
-    sim_results = likelihood_fn.simulator.run(np.linspace(0, 5000, 100))
+    sim_results = likelihood_fn.simulator.run()
 
     # measurement
     likelihood_fn.measurement_model.update_simulation_result(sim_results)
@@ -65,3 +65,4 @@ def likelihood_fn(x):
     print(x)
     print(l)
     return l
+
